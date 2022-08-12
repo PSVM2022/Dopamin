04cf7fafd5f1c1b4671afc675ec21f5e6e128ed8
ombujeong <ombujeong@gmail.com>
2022-08-12 19:52:11 +0900

    feat: 평가된 한줄평 모아보기 controller

--- src/main/java/com/PSVM/dopamin/controller/MyPageController.java
+++ src/main/java/com/PSVM/dopamin/controller/MyPageController.java
@@ -2,15 +2,11 @@ package com.PSVM.dopamin.controller;
 
 import com.PSVM.dopamin.domain.RevwDto;
 import com.PSVM.dopamin.service.MyPageService;
-import com.PSVM.dopamin.service.MyPageServiceImple;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -19,13 +15,22 @@ public class MyPageController {
     @Autowired
     MyPageService MyPageService;
 
+    //한줄평 리스트 가져오기
     @GetMapping("/revw/{userId}")
-    public String viewRevw(@PathVariable String userId, Model m) throws Exception{
+    public String readRevwList(@PathVariable String userId, Model m) throws Exception{
         List<RevwDto> revwDtoList = MyPageService.revwList(userId);
         m.addAttribute("revwDtoList", revwDtoList);
         System.out.println("revwDtoList = " + revwDtoList);
         return "home";
+    }
 
+    // 평가된 한줄평 가져오기
+    @GetMapping("/revweval/{user_id}")
+    public String readRevwEvalList(@PathVariable String user_id, Model m) throws Exception{
+        List<RevwDto> revwDtoList = MyPageService.revwEvalList(user_id);
+        m.addAttribute("revwDtoList", revwDtoList);
+        System.out.println("revwDtoList = " + revwDtoList);
+        return "home";
     }
 
 
