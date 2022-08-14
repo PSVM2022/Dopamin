33fb78a18573e1b1e32fbe96d948ae77ab27c421
ombujeong <ombujeong@gmail.com>
2022-08-15 06:41:08 +0900

    feat : MyPage 메인화면 추가 - Service
    myPage 메인 화면에 필요한 유저 정보 가져오는 Service 추가

--- src/main/java/com/PSVM/dopamin/service/MyPageService.java
+++ src/main/java/com/PSVM/dopamin/service/MyPageService.java
@@ -1,13 +1,14 @@
 package com.PSVM.dopamin.service;
 
 import com.PSVM.dopamin.domain.MyPageCntsDto;
+import com.PSVM.dopamin.domain.MyPageDto;
 import com.PSVM.dopamin.domain.MyPagePostDto;
 import com.PSVM.dopamin.domain.RevwDto;
 
 import java.util.List;
 
 public interface MyPageService {
-    List<RevwDto> revwList(String userId) throws Exception;
+    List<RevwDto> revwList(String user_id) throws Exception;
 
     List<RevwDto> revwEvalList(String user_id);
 
@@ -16,4 +17,6 @@ public interface MyPageService {
     List<MyPageCntsDto> cntsViewList(String user_id);
 
     List<MyPagePostDto> postList(String user_id);
+
+    MyPageDto selectMyInfo(String user_id);
 }
--- src/main/java/com/PSVM/dopamin/service/MyPageServiceImple.java
+++ src/main/java/com/PSVM/dopamin/service/MyPageServiceImple.java
@@ -2,11 +2,13 @@ package com.PSVM.dopamin.service;
 
 import com.PSVM.dopamin.dao.MyPageDao;
 import com.PSVM.dopamin.domain.MyPageCntsDto;
+import com.PSVM.dopamin.domain.MyPageDto;
 import com.PSVM.dopamin.domain.MyPagePostDto;
 import com.PSVM.dopamin.domain.RevwDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -15,8 +17,8 @@ public class MyPageServiceImple implements MyPageService {
     MyPageDao myPageDao;
 
     @Override
-    public List<RevwDto> revwList(String userId) throws Exception {
-        return myPageDao.selectRevw(userId);
+    public List<RevwDto> revwList(String user_id) throws Exception {
+        return myPageDao.selectRevw(user_id);
     }
 
     @Override
@@ -37,6 +39,19 @@ public class MyPageServiceImple implements MyPageService {
     @Override
     public List<MyPagePostDto> postList(String user_id) {
         return myPageDao.selectPost(user_id);
+    }
+
+    @Override
+    public MyPageDto selectMyInfo(String user_id) {
+        MyPageDto myPageDto = myPageDao.selectMyInfo(user_id);
+
+        //유저의 생년월일 정보를 받아서 나이값 구하고 myPageDto에 넣어주기
+        int birthYear = myPageDto.getBtdt().getYear() + 1900;
+        int curYear = LocalDate.now().getYear();
+        int age = (int) (curYear - birthYear + 1) / 10 * 10;
+        myPageDto.setAge(age);
+
+        return myPageDto;
     }
 
 }
