c397cc4d56c51d63206a89e92134572d52e269a7
ombujeong <ombujeong@gmail.com>
2022-08-18 18:15:36 +0900

    fix: MyPageController 에서 session으로 id 받기 수정
    session 으로 id 받을 수 있게 추가

--- src/main/java/com/PSVM/dopamin/controller/MyPageController.java
+++ src/main/java/com/PSVM/dopamin/controller/MyPageController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -19,27 +20,30 @@ public class MyPageController {
     MyPageService myPageService;
 
     // MyPage 메인 화면
-    @GetMapping("/{user_id}")
-    public String myPage(@PathVariable String user_id,Model m) throws Exception {
+    @GetMapping("")
+    public String myPage(HttpSession session, Model m) throws Exception {
+        String user_id = (String) session.getAttribute("id");
         MyPageDto myPageDto = myPageService.selectMyInfo(user_id);
-        m.addAttribute("user_id", user_id);
-        m.addAttribute("myPageDto",myPageDto);
+//        m.addAttribute("user_id", user_id);  <c:url 태그에서 session에 있는 id를 쓸 수 있으면 굳이 모델에 담지 않아도 된다.
+        m.addAttribute("myPageDto", myPageDto);
         return "myPageMain";
     }
 
     //한줄평 리스트 가져오기
-    @GetMapping("/revw/{user_id}")
-    public String readRevwList(@PathVariable String user_id, Model m) throws Exception {
+    @GetMapping("/revw")
+    public String readRevwList(HttpSession session, Model m) throws Exception {
+        String user_id = (String) session.getAttribute("id");
         List<RevwDto> revwDtoList = myPageService.revwList(user_id);
-        m.addAttribute("user_id",user_id);
+//        m.addAttribute("user_id",user_id);
         m.addAttribute("revwDtoList", revwDtoList);
         System.out.println("revwDtoList = " + revwDtoList);
         return "revw";
     }
 
     // 평가된 한줄평 가져오기
-    @GetMapping("/revweval/{user_id}")
-    public String readRevwEvalList(@PathVariable String user_id, Model m) throws Exception {
+    @GetMapping("/revweval")
+    public String readRevwEvalList(HttpSession session, Model m) throws Exception {
+        String user_id = (String) session.getAttribute("id");
         List<RevwDto> revwDtoList = myPageService.revwEvalList(user_id);
         m.addAttribute("revwDtoList", revwDtoList);
         System.out.println("revwDtoList = " + revwDtoList);
@@ -47,8 +51,9 @@ public class MyPageController {
     }
 
     //컨텐츠 찜 목록 가져오기
-    @GetMapping("/cntswish/{user_id}")
-    public String readCntsWishList(@PathVariable String user_id, Model m) throws Exception {
+    @GetMapping("/cntswish")
+    public String readCntsWishList(HttpSession session, Model m) throws Exception {
+        String user_id = (String) session.getAttribute("id");
         List<MyPageCntsDto> cntsDtoList = myPageService.cntsWishList(user_id);
         m.addAttribute("cntsDtoList", cntsDtoList);
         System.out.println("cntsDtoList = " + cntsDtoList);
@@ -56,8 +61,9 @@ public class MyPageController {
     }
 
     //컨텐츠 조회 기록 목록 가져오기
-    @GetMapping("/cntsview/{user_id}")
-    public String readCntsViewList(@PathVariable String user_id, Model m) throws Exception {
+    @GetMapping("/cntsview")
+    public String readCntsViewList(HttpSession session, Model m) throws Exception {
+        String user_id = (String) session.getAttribute("id");
         List<MyPageCntsDto> cntsDtoList = myPageService.cntsViewList(user_id);
         m.addAttribute("cntsDtoList", cntsDtoList);
         System.out.println("cntsDtoList = " + cntsDtoList);
@@ -65,8 +71,9 @@ public class MyPageController {
     }
 
     //게시글 목록 가져오기
-    @GetMapping("/post/{user_id}")
-    public String readPostList(@PathVariable String user_id, Model m) throws Exception {
+    @GetMapping("/post")
+    public String readPostList(HttpSession session, Model m) throws Exception {
+        String user_id = (String) session.getAttribute("id");
         List<MyPagePostDto> postDtoList = myPageService.postList(user_id);
         m.addAttribute("postDtoList", postDtoList);
         return "post";
