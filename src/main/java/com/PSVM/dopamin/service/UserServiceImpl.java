a6579ceae8e6b69c28b6cdd59e83074912ac308e
hyoeun0101 <kimhyoeun0101@gmail.com>
2022-08-18 22:08:22 +0900

    feat: 회원가입 UserService 작성 중

--- src/main/java/com/PSVM/dopamin/service/UserServiceImpl.java
+++ src/main/java/com/PSVM/dopamin/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.PSVM.dopamin.dao.UserDaoImpl;
 import com.PSVM.dopamin.domain.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,12 +18,13 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public int userJoin(UserDto userDto) {
+        //비밀번호 암호화 메서드 호출 필요
 
         userDao.insertUser(userDto);
         userDao.insertUserPwd(userDto);
         return 0;
     }
-
 
 }
