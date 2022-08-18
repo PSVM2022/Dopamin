dc3c4888134e1747562902d32e014cb17940d366
ombujeong <ombujeong@gmail.com>
2022-08-18 17:56:28 +0900

    fix: 변수명 수정
    myPageService 주입받은 변수명 수정함.

--- src/main/java/com/PSVM/dopamin/controller/MyPageController.java
+++ src/main/java/com/PSVM/dopamin/controller/MyPageController.java
@@ -16,12 +16,12 @@ import java.util.List;
 @RequestMapping("/mypage")
 public class MyPageController {
     @Autowired
-    MyPageService MyPageService;
+    MyPageService myPageService;
 
     // MyPage 메인 화면
     @GetMapping("/{user_id}")
     public String myPage(@PathVariable String user_id,Model m) throws Exception {
-        MyPageDto myPageDto = MyPageService.selectMyInfo(user_id);
+        MyPageDto myPageDto = myPageService.selectMyInfo(user_id);
         m.addAttribute("user_id", user_id);
         m.addAttribute("myPageDto",myPageDto);
         return "myPageMain";
@@ -30,7 +30,7 @@ public class MyPageController {
     //한줄평 리스트 가져오기
     @GetMapping("/revw/{user_id}")
     public String readRevwList(@PathVariable String user_id, Model m) throws Exception {
-        List<RevwDto> revwDtoList = MyPageService.revwList(user_id);
+        List<RevwDto> revwDtoList = myPageService.revwList(user_id);
         m.addAttribute("user_id",user_id);
         m.addAttribute("revwDtoList", revwDtoList);
         System.out.println("revwDtoList = " + revwDtoList);
@@ -40,7 +40,7 @@ public class MyPageController {
     // 평가된 한줄평 가져오기
     @GetMapping("/revweval/{user_id}")
     public String readRevwEvalList(@PathVariable String user_id, Model m) throws Exception {
-        List<RevwDto> revwDtoList = MyPageService.revwEvalList(user_id);
+        List<RevwDto> revwDtoList = myPageService.revwEvalList(user_id);
         m.addAttribute("revwDtoList", revwDtoList);
         System.out.println("revwDtoList = " + revwDtoList);
         return "revweval";
@@ -49,7 +49,7 @@ public class MyPageController {
     //컨텐츠 찜 목록 가져오기
     @GetMapping("/cntswish/{user_id}")
     public String readCntsWishList(@PathVariable String user_id, Model m) throws Exception {
-        List<MyPageCntsDto> cntsDtoList = MyPageService.cntsWishList(user_id);
+        List<MyPageCntsDto> cntsDtoList = myPageService.cntsWishList(user_id);
         m.addAttribute("cntsDtoList", cntsDtoList);
         System.out.println("cntsDtoList = " + cntsDtoList);
         return "cntswish";
@@ -58,7 +58,7 @@ public class MyPageController {
     //컨텐츠 조회 기록 목록 가져오기
     @GetMapping("/cntsview/{user_id}")
     public String readCntsViewList(@PathVariable String user_id, Model m) throws Exception {
-        List<MyPageCntsDto> cntsDtoList = MyPageService.cntsViewList(user_id);
+        List<MyPageCntsDto> cntsDtoList = myPageService.cntsViewList(user_id);
         m.addAttribute("cntsDtoList", cntsDtoList);
         System.out.println("cntsDtoList = " + cntsDtoList);
         return "cntsview";
@@ -67,7 +67,7 @@ public class MyPageController {
     //게시글 목록 가져오기
     @GetMapping("/post/{user_id}")
     public String readPostList(@PathVariable String user_id, Model m) throws Exception {
-        List<MyPagePostDto> postDtoList = MyPageService.postList(user_id);
+        List<MyPagePostDto> postDtoList = myPageService.postList(user_id);
         m.addAttribute("postDtoList", postDtoList);
         return "post";
     }
