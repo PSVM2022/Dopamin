db3fc9d0aae2e58112a5effac2533e188180234c
sohyeonnn <shyeon02060@gmail.com>
2022-08-18 10:38:53 +0900

    feat: 한줄평 작성중.
    한줄평 CRUD작성중.

--- src/main/java/com/PSVM/dopamin/controller/ReviewController.java
+++ src/main/java/com/PSVM/dopamin/controller/ReviewController.java
@@ -1,11 +1,12 @@
 package com.PSVM.dopamin.controller;
 
+import com.PSVM.dopamin.domain.ContentsDto;
 import com.PSVM.dopamin.domain.ReviewDto;
 import com.PSVM.dopamin.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -15,15 +16,16 @@ public class ReviewController {
     @Autowired
     ReviewService reviewService;
 
-    @ResponseBody
+
     @GetMapping("/review")
-    public List<ReviewDto> list(Integer cnts_id) {
-        List<ReviewDto> list = null;
-        try {
-            list = reviewService.getRevwList(cnts_id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
+    public String getRevwList(Integer cnts_id, Model model) {
+        //List<ReviewDto> list = null;
+
+        List<ReviewDto> reviewDtoList = reviewService.getRevwList(cnts_id);
+
+        model.addAttribute("reviewDtoList", reviewDtoList);
+
+
+        return "contentsView";
     }
 }
