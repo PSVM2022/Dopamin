1cc5fc544f9e5d0280f4c1937eaf4efe4125a61f
hyoeun0101 <kimhyoeun0101@gmail.com>
2022-08-21 00:44:48 +0900

    fix: 로그인 로직 수정

--- src/main/java/com/PSVM/dopamin/controller/LoginController.java
+++ src/main/java/com/PSVM/dopamin/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.PSVM.dopamin.controller;
 import com.PSVM.dopamin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,11 @@ import java.net.URLEncoder;
 public class LoginController {
     @Autowired
     UserService userService;
+
+//    @ExceptionHandler(NullPointerException.class)
+//    public String catcher(){
+//
+//    }
 
     @GetMapping("/login")
     public String loginForm(HttpServletRequest request,String toURL){
@@ -41,14 +47,14 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(String id, String pwd, String toURL, HttpServletRequest request) throws Exception {
-
-        //id, pwd 불일치, 로그인 실패
-//        if(!idPwdCheck(id,pwd)){
-//            String msg = URLEncoder.encode("아이디 또는 비밀번호를 잘못 입력했습니다.","utf-8");
+//        id, pwd 불일치, 로그인 실패
+        if(!userService.idPwdCheck(id,pwd)){
+            String msg = URLEncoder.encode("아이디 또는 비밀번호를 잘못 입력했습니다.","utf-8");
 //            return "redirect:/login/login?msg="+msg;
-//        }
-        //id, pwd 일치하면 로그인 성공
-        //세션 생성
+            return "forward:/login/login?msg="+msg+"&id="+id;
+        }
+//        id, pwd 일치하면 로그인 성공
+//        세션 생성
         HttpSession session = request.getSession();
         //  세션 객체에 id를 저장
         session.setAttribute("id", id);
@@ -69,11 +75,5 @@ public class LoginController {
     }
 
 
-//    private boolean idPwdCheck(String id, String pwd){
-//        //존재하지 않는 아이디면
-//        if(userService.getUserPwd(id)==null){
-//            return false;
-//        }
-//        return userService.getUserPwd(id).getUser_pwd().equals(pwd);
-//    }
+
 }
