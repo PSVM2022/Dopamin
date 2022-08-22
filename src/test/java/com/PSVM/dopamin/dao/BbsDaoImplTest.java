f16bb58dfb5f42c9c78a08070000bed07feaece6
ellery <public.stevemoon@gmail.com>
2022-08-23 06:09:03 +0900

    feat: 게시글 Dao 인터페이스,구현체 작성, 구현체에 대한 유닛테스트 작성
    게시글 DAO 구현체 작성함. 기능 상 필요없거나 사용빈도가 적은 쿼리는 지움(게시판 삭제 등)

--- src/test/java/com/PSVM/dopamin/dao/BbsDaoImplTest.java
+++ src/test/java/com/PSVM/dopamin/dao/BbsDaoImplTest.java
@@ -1,73 +1,130 @@
-//package com.PSVM.dopamin.dao;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotEquals;
-//import static org.junit.Assert.assertNull;
-//import static org.junit.Assert.assertTrue;
-//
-//import com.PSVM.dopamin.domain.BbsDto;
-//import java.sql.Timestamp;
-//import org.junit.FixMethodOrder;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.junit.runners.MethodSorters;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-//public class BbsDaoImplTest {
-//
-//    @Autowired
-//    BbsDao bbsDao;
-//
-//    @Test
-//    public void t1_connectDb() {
-//        assertNotEquals(bbsDao, null);
-//    }
-//
-////    @Test
-////    public void t2_insert() throws Exception {
-////        bbsDao.deleteAllByManager();
-////
-////        String[] names = {"창무", "효은", "소현", "동훈", "은비"};
-////        for (int i = 0; i < 5; ++i) {
-////            BbsDto bbsDto = new BbsDto(names[i] + "의 게시판", i, new Timestamp(System.currentTimeMillis()), names[i], names[i]);
-////            bbsDao.insert(bbsDto);
-////        }
-////        assertTrue(bbsDao.count() == names.length);
-////    }
-//
-//    @Test
-//    public void t3_select() throws Exception {
-//        BbsDto target = bbsDao.select(0);
-//        assertEquals(target.getBbs_nm(), "창무의 게시판");
-//    }
-//
-//    @Test
-//    public void t4_update() throws Exception {
-//        BbsDto target = bbsDao.select(4);
-//        String newTitle = "여기는 은비땅이다~ 게시판입니다";
-//        target.setBbs_nm(newTitle);
-//        bbsDao.update(target);
-//
-//        BbsDto nTarget = bbsDao.select(4);
-//        assertEquals(nTarget.getBbs_nm(), newTitle);
-//    }
-//
-//    @Test
-//    public void t5_deleteByManager() throws Exception {
-//        bbsDao.deleteByManager(4);
-//        assertNull(bbsDao.select(4));
-//
-//        BbsDto bbsDto = new BbsDto("은비의 게시판", 4, new Timestamp(System.currentTimeMillis()), "은비", "은비");
-//        bbsDao.insert(bbsDto);
-//    }
-//
-//    @Test
-//    public void t6_count() throws Exception {
-//        assertTrue(bbsDao.count() > 0);
-//    }
-//}
+package com.PSVM.dopamin.dao;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import com.PSVM.dopamin.domain.BbsDto;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class BbsDaoImplTest {
+
+    @Autowired
+    BbsDao bbsDao;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void checkDbStatus() {
+        assertNotNull(bbsDao);
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void shouldSaveTempBbsWhenNoBbsParameter() throws SQLException {
+        BbsDto bbs = new BbsDto();
+        bbsDao.save(bbs);
+        BbsDto target = (BbsDto) bbsDao.findAll().get(bbsDao.count() - 1);
+
+        System.out.println(target);
+        assertTrue(target.getBbsNm().startsWith("임시 판#"));
+        assertEquals(target.getInUser(), "관리자");
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void shouldSaveBbs() throws SQLException {
+        BbsDto bbs = new BbsDto("은비까비", "창무");
+        bbsDao.save(bbs);
+        BbsDto target = (BbsDto) bbsDao.findAll().get(bbsDao.count() - 1);
+
+        assertArrayEquals(
+            new Object[]{
+                target.getBbsNm(),
+                target.getBbsPrior(),
+                target.getUpUser(),
+                target.getInUser()
+            },
+            new Object[]{
+                "은비까비",
+                0,
+                "창무",
+                "창무"
+            }
+        );
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void shouldSaveAllBbsWhenBbsGiven() throws SQLException {
+        int defaultLen = bbsDao.count();
+        for (int i = 0; i < 10; ++i) {
+            bbsDao.save(new BbsDto());
+        }
+
+        assertEquals(defaultLen + 10, bbsDao.count());
+    }
+
+    @Test
+    public void shouldFindBbsById() throws SQLException {
+        Optional<BbsDto> target = bbsDao.findById(10);
+        if (target.isPresent()) {
+            assertNotNull(target.get().getBbsId());
+            return;
+        }
+        assertNull(target.get());
+    }
+
+    @Test
+    public void shouldFindBbsByPrefixName() throws SQLException {
+        BbsDto target = bbsDao.findByPrefixName("일일");
+        assertEquals(target.getBbsNm(), "일일베스트");
+    }
+
+    @Test
+    public void shouldFindAllVisibBbsAsList() throws SQLException {
+        List list = bbsDao.findAllVisib();
+        System.out.println(list);
+
+        assertNotNull(list.get(0));
+        assertEquals(bbsDao.count(), list.size());
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void shouldUpdateBbsInfo() throws SQLException {
+        BbsDto target = bbsDao.findByPrefixName("일일");
+        int id = target.getBbsId();
+        target.setBbsVisib(0);
+        target.setBbsNm("test name");
+        bbsDao.update(target);
+
+        Optional<BbsDto> nBbs = bbsDao.findById(id);
+        assertEquals(nBbs.get().getBbsNm(), "test name");
+    }
+}
