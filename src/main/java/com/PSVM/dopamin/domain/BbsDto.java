fb18b631d2a23842e1bb283329703de4d951a8c4
ellery <public.stevemoon@gmail.com>
2022-08-23 06:00:29 +0900

    fix: 게시글 DTO 개선
    따로 변수명 변환 로직을 넣지 않아서 db의 컬럼명 규칙인 snake case로 통일시킴, 필드 타입 정정

--- src/main/java/com/PSVM/dopamin/domain/BbsDto.java
+++ src/main/java/com/PSVM/dopamin/domain/BbsDto.java
@@ -6,28 +6,51 @@ import java.util.Random;
 
 public class BbsDto {
 
-    private Integer bbsId;
-    private String bbsNm;
-    private Integer bbsPrior;
-    private Timestamp inDate;
-    private String inUser;
-    private Timestamp upDate;
-    private String upUser;
+    private Integer bbs_id;
+    private String bbs_nm;
+    private Integer bbs_prior;
+    private Integer bbs_visib;
+    private Timestamp in_date;
+    private String in_user;
+    private Timestamp up_date;
+    private String up_user;
 
-    BbsDto(String bbsNm, Integer bbsPrior, String editorNm) {
-        this.bbsNm = bbsNm;
-        this.bbsPrior = bbsPrior;
-        this.inDate = new Timestamp(System.currentTimeMillis());
-        this.inUser = this.upUser = editorNm;
+    public BbsDto(
+        Integer bbs_id,
+        String bbs_nm,
+        Integer bbs_prior,
+        Integer bbs_visib,
+        Timestamp in_date,
+        String in_user,
+        Timestamp up_date,
+        String up_user
+    ) {
+        this.bbs_id = bbs_id;
+        this.bbs_nm = bbs_nm;
+        this.bbs_prior = bbs_prior;
+        this.bbs_visib = bbs_visib;
+        this.in_date = in_date;
+        this.in_user = in_user;
+        this.up_date = up_date;
+        this.up_user = up_user;
     }
-    BbsDto(String bbsNm, String editorNm) {
+
+    public BbsDto(String bbsNm, Integer bbsPrior, String editorNm) {
+        this.bbs_nm = bbsNm;
+        this.bbs_prior = bbsPrior;
+        this.bbs_visib = 1;
+        this.in_date = new Timestamp(System.currentTimeMillis());
+        this.in_user = this.up_user = editorNm;
+    }
+
+    public BbsDto(String bbsNm, String editorNm) {
         this(bbsNm, 0, editorNm);
     }
 
-    /*  테스트용으로만 쓰이는 생성자 */
-    BbsDto() {
-        this("임시 판 #" + getRandomHexStr(), "관리자");
+    public BbsDto() {
+        this("임시 판#" + getRandomHexStr(), "관리자");
     }
+
 
     private static String getRandomHexStr() {
         StringBuilder ret = new StringBuilder();
@@ -40,43 +63,68 @@ public class BbsDto {
         return ret.toString();
     }
 
-    public int getBbsId() {
-        return bbsId;
+    public Integer getBbsId() {
+        return bbs_id;
+    }
+
+    public void setBbsId(Integer bbsId) {
+        this.bbs_id = bbsId;
     }
 
     public String getBbsNm() {
-        return bbsNm;
+        return bbs_nm;
     }
 
-    public void setBbsNm(String bbsNm, String editorNm) {
-        this.bbsNm = bbsNm;
-        this.upUser = editorNm;
+    public void setBbsNm(String bbsNm) {
+        this.bbs_nm = bbsNm;
     }
 
     public int getBbsPrior() {
-        return bbsPrior;
+        return bbs_prior;
     }
 
-    public void setBbsPrior(Integer bbsPrior, String editorNm) {
-        this.bbsPrior = bbsPrior;
-        this.upUser = editorNm;
+    public void setBbsPrior(int bbsPrior) {
+        this.bbs_prior = bbsPrior;
+    }
+
+    public Integer getBbsVisib() {
+        return bbs_visib;
+    }
+
+    public void setBbsVisib(Integer bbsVisib) {
+        this.bbs_visib = bbsVisib;
     }
 
     public Timestamp getInDate() {
-        return inDate;
+        return in_date;
+    }
+
+    public void setInDate(Timestamp inDate) {
+        this.in_date = inDate;
     }
 
     public String getInUser() {
-        return inUser;
+        return in_user;
     }
 
+    public void setInUser(String inUser) {
+        this.in_user = inUser;
+    }
+
+    public Timestamp getUpDate() {
+        return up_date;
+    }
+
+    public void setUpDate(Timestamp upDate) {
+        this.up_date = upDate;
+    }
 
     public String getUpUser() {
-        return upUser;
+        return up_user;
     }
 
     public void setUpUser(String upUser) {
-        this.upUser = upUser;
+        this.up_user = upUser;
     }
 
     @Override
@@ -88,24 +136,26 @@ public class BbsDto {
             return false;
         }
         BbsDto bbsDto = (BbsDto) o;
-        return bbsNm.equals(bbsDto.bbsNm);
+        return bbs_id.equals(bbsDto.bbs_id) && bbs_nm.equals(bbsDto.bbs_nm) && bbs_prior.equals(
+            bbsDto.bbs_prior);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(bbsNm);
+        return Objects.hash(bbs_id, bbs_nm, bbs_prior);
     }
 
     @Override
     public String toString() {
         return "BbsDto{" +
-            "bbsId=" + bbsId +
-            ", bbsNm='" + bbsNm + '\'' +
-            ", bbsPrior=" + bbsPrior +
-            ", inDate=" + inDate +
-            ", inUser='" + inUser + '\'' +
-            ", upDate='" + upDate +
-            ", upUser='" + upUser + '\'' +
-            '}';
+            "bbs_id=" + bbs_id +
+            ", bbs_nm='" + bbs_nm + '\'' +
+            ", bbs_prior=" + bbs_prior +
+            ", bbs_visible=" + bbs_visib +
+            ", in_date=" + in_date +
+            ", in_user='" + in_user + '\'' +
+            ", up_date=" + up_date +
+            ", up_user='" + up_user + '\'' +
+            '}' + "\n";
     }
 }
