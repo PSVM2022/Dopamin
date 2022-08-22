c7895dc3880527bd2e69e8eacdfaad534420600b
ombujeong <ombujeong@gmail.com>
2022-08-22 16:23:34 +0900

    feature : 장르 이름 추가

--- src/main/java/com/PSVM/dopamin/domain/MyPageDto.java
+++ src/main/java/com/PSVM/dopamin/domain/MyPageDto.java
@@ -6,20 +6,22 @@ public class MyPageDto {
     private String nic;     //닉네임
     private Integer dopa_exp;   //도파 exp
     private Integer dopa_point; //도파 포인트
-    private String fav_genre1;    // 선호 장르
+    private Integer fav_genre1;    // 선호 장르 번호
+    private String genre_nm;        //선호 장르 이름
     private String btdt;           //유저 생년월일
     private Integer age;        // 유저 나이
     private Integer sex;        // 유저 성별
     private String item_img;    // 착용중인 보유 스킨
 
     public MyPageDto(){}
-    public MyPageDto(String user_id, String prf_img, String nic, Integer dopa_exp, Integer dopa_point, String fav_genre1, String btdt, Integer age, Integer sex, String item_img) {
+    public MyPageDto(String user_id, String prf_img, String nic, Integer dopa_exp, Integer dopa_point, Integer fav_genre1, String genre_nm, String btdt, Integer age, Integer sex, String item_img) {
         this.user_id = user_id;
         this.prf_img = prf_img;
         this.nic = nic;
         this.dopa_exp = dopa_exp;
         this.dopa_point = dopa_point;
         this.fav_genre1 = fav_genre1;
+        this.genre_nm = genre_nm;
         this.btdt = btdt;
         this.age = age;
         this.sex = sex;
@@ -66,12 +68,20 @@ public class MyPageDto {
         this.dopa_point = dopa_point;
     }
 
-    public String getFav_genre1() {
+    public Integer getFav_genre1() {
         return fav_genre1;
     }
 
-    public void setFav_genre1(String fav_genre1) {
+    public void setFav_genre1(Integer fav_genre1) {
         this.fav_genre1 = fav_genre1;
+    }
+
+    public String getGenre_nm() {
+        return genre_nm;
+    }
+
+    public void setGenre_nm(String genre_nm) {
+        this.genre_nm = genre_nm;
     }
 
     public String getBtdt() {
