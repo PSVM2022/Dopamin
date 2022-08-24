e01376b3d30044226c0dcdd8d1967e57ae32bc85
hyoeun0101 <kimhyoeun0101@gmail.com>
2022-08-24 22:48:19 +0900

    style: loginForm 디자인 수정

--- src/main/java/com/PSVM/dopamin/controller/LoginController.java
+++ src/main/java/com/PSVM/dopamin/controller/LoginController.java
@@ -8,11 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 @Controller
@@ -46,21 +48,25 @@ public class LoginController {
         try{
             //아이디, 비밀번호 일치 확인
             boolean result = userService.idPwdCheck(id,pwd);
+
             if(!result){
-                throw new NullPointerException("존재하지 않는 유저입니다.");
+                throw new NullPointerException("아이디와 비밀번호가 일치하지 않습니다.");
             }
+            //아이디와 비번 일치하지 않거나, 존재하지 않은 아이디일 때 처리
         }catch (NullPointerException ne){
-            String msg = URLEncoder.encode(" 아이디 또는 비밀번호를 잘못 입력했습니다.\n" +
-                    "입력하신 내용을 다시 확인해주세요.","utf-8");
-            model.addAttribute("id",id);
+            String msg = " 아이디 또는 비밀번호를 잘못 입력했습니다.\n" +
+                    "입력하신 내용을 다시 확인해주세요.";
             model.addAttribute("msg",msg);
-            return "redirect:/login/login";
+            return "Login/loginForm";
         }
+
+        System.out.println("넘어오지마!!");
 //        id, pwd 일치하면 로그인 성공 ,세션 생성
         HttpSession session = request.getSession();
         //  세션 객체에 저장할 정보 - user_id, 장바구니_id, 유저 상태(관리자인지 일반유저인지), 설문조사 했는지 안했는지 여부
         UserDto user = userService.getUser(id);
         session.setAttribute("USERID", id);
+        System.out.println("login success!!. USERID = "+session.getAttribute("USERID"));
         String cartId = userService.getCartId(id);
         session.setAttribute("CARTID",cartId);
         session.setAttribute("USERSTAT",user.getUser_stat());
@@ -80,7 +86,7 @@ public class LoginController {
             response.addCookie(cookie);
         }
         if(rememberLogin){
-            session.setMaxInactiveInterval(60);
+            session.setMaxInactiveInterval(60*60*24*7);
         }
         toURL = toURL==null || toURL.equals("") ? "/" : toURL;
         return "redirect:"+toURL;
