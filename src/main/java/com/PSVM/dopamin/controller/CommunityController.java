f2e07974a1c7de76ffa30c0f78a5fb14acb76c18
Changmoo Moon <public.stevemoon@gmail.com>
2022-08-30 23:08:26 +0900

    feat: 커뮤니티 컨트롤러(게시판, 게시글) 메서드 추가
    게시판, 게시글 뷰에 필요한 서비스 호출, 모델값 적용
    게시글에서 게시판을 다시 보여줘야함

--- src/main/java/com/PSVM/dopamin/controller/CommunityController.java
+++ src/main/java/com/PSVM/dopamin/controller/CommunityController.java
@@ -6,15 +6,16 @@ import com.PSVM.dopamin.domain.community.BbsDto;
 import com.PSVM.dopamin.domain.community.PostDto;
 import com.PSVM.dopamin.service.community.CommunityService;
 import com.PSVM.dopamin.service.community.PostService;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -36,16 +37,12 @@ public class CommunityController {
 
     @GetMapping("/community/{bbsId}")
     public String board(
-        @PathVariable("bbsId") Integer bbsId,
+        @PathVariable Integer bbsId,
         @RequestParam(required = false, defaultValue = "1") Integer page,
         Model m
     ) throws Exception {
         try {
-            Map map = new HashMap<>();
-            map.put("bbsId", bbsId);
-            map.put("page", page);
-
-            List<PostDto> list = postService.getPage(map);
+            List<PostDto> list = postService.getPage(bbsId, page);
             String bbsNm = postService.getBbsName(bbsId);
 
             m.addAttribute("postList", list);
@@ -63,9 +60,51 @@ public class CommunityController {
     }
 
     @GetMapping("/post/{postId}")
-    public String post(@PathVariable("postId") int postId) {
-        System.out.println("저는 포스트 페이지 " + postId + " 입니다");
+    public String post(
+        @PathVariable int postId,
+        @RequestParam(required = false, defaultValue = "1") Integer page,
+        Model m,
+        HttpServletRequest req,
+        HttpSession session
+    ) {
+        try {
+            PostDto post = postService.read(postId);
+            Integer bbsId = post.getBbs_id();
+            String bbsNm = postService.getBbsNameByPostId(postId);
+            String nowUrl = String.valueOf(req.getRequestURL());
+            int totalCnt = postService.getCountInBbs(bbsId);
+            PageHandler ph = new PageHandler(totalCnt, new SearchCondition(page, 25, ""));
+            List<PostDto> list = postService.getPage(bbsId, page);
+
+            m.addAttribute("post", post);
+            m.addAttribute("bbsId", bbsId);
+            m.addAttribute("bbsNm", bbsNm);
+            m.addAttribute("nowUrl", nowUrl);
+            m.addAttribute("postList", list);
+            m.addAttribute("ph", ph);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return "Community/communityPost";
+    }
+
+    // TODO: writer session 추가
+    @PostMapping("/post/remove/{postId}")
+    public String remove(@PathVariable Integer postId, Model m, HttpSession session)
+        throws Exception {
+//        String writer = session.getAttribute("id");
+        String writer = "user1";
+        Integer bbsId = postService.read(postId).getBbs_id();
+
+        try {
+            postService.remove(postId, writer);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return "redirect:/community/" + bbsId;
     }
 
     @GetMapping("/post/edit")
